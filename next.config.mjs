178844546ea98edf846b130264d565177f92083f
iceiec <isaiah.aguinaldo2@gmail.com>
2026-01-27 11:06:07 +0800

    Configure Next.js for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
